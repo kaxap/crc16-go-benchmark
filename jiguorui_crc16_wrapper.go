258d254c5ef6c3925d286511125ff74646337c54
kaxapi <kashimov@gmail.com>
2019-06-01 01:33:35 +0600

    add credits to jiguorui wrapper

--- jiguorui_crc16_wrapper.go
+++ jiguorui_crc16_wrapper.go
@@ -1,5 +1,6 @@
 package crc16ccitt
 
+// adapted from https://github.com/jiguorui/crc16
 var MbTable = []uint16{
 	0X0000, 0XC0C1, 0XC181, 0X0140, 0XC301, 0X03C0, 0X0280, 0XC241,
 	0XC601, 0X06C0, 0X0780, 0XC741, 0X0500, 0XC5C1, 0XC481, 0X0440,
